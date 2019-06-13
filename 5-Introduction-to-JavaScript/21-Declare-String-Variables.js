3963f77897406a7394eaea6dde9e5f6674971ff6
Lormaniac <sean.lorman@gmail.com>
2019-06-12 22:28:21 -0600

    Created two literal string variables containing my first and last name

---
+++ 5-Introduction-to-JavaScript/21-Declare-String-Variables.js
@@ -0,0 +1,8 @@
+// zero or more characters enclosed in '' or "" is a literal string. You can create variables with string values.
+// Example
+var firstName = "Alan";
+var lastName = "Turing";
+
+// Only change code below this line
+var myFirstName = "Sean";
+var myLastName = "Lorman";
