51a0457d069562c30a4a016936b84916b7373b80
Lormaniac <sean.lorman@gmail.com>
2019-06-12 22:01:46 -0600

    Used the -= operator

---
+++ 5-Introduction-to-JavaScript/18-Compound-Assignment-With-Augmented-Subtraction.js
@@ -0,0 +1,10 @@
+// The -= operator subtracts a number from a variable. In this exercise it showed that it doesn't work well with negative numbers and you will need to remove the negative infront of the number.
+var a = 11;
+var b = 9;
+var c = 3;
+
+// Only modify code below this line
+
+a -= 6;
+b -= 15;
+c -= 1;
