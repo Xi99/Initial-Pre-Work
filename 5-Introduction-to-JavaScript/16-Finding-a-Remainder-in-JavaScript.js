f663782e3223675d614f59c83c3445ad3c5cb98f
Lormaniac <sean.lorman@gmail.com>
2019-06-12 21:46:04 -0600

    Used the remainder module

---
+++ 5-Introduction-to-JavaScript/16-Finding-a-Remainder-in-JavaScript.js
@@ -0,0 +1,4 @@
+// The remainder module "%" is used to get the remainder of two divided numbers. This can be used to check if a number is even or odd by seeing if the remainder is 0 or 1.
+// Only change code below this line
+
+var remainder = 11 % 3;
