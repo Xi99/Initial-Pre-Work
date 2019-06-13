51562c084b0c9da5c2f590f6e3f42207a4c7fd73
Lormaniac <sean.lorman@gmail.com>
2019-06-12 20:19:19 -0600

    assigned a = 7 and b = a

---
+++ 5-Introduction-to-JavaScript/3-Storing-Values-with-the-Assignment-Operator.js
@@ -0,0 +1,8 @@
+// The assignment operator (=) is used to store a value in the variable. The assignments are read from right to left.
+// Setup
+var a;
+var b = 2;
+
+// Only change code below this line
+a = 7;
+b = a;
