643c747d834cc85d1cb4926f9c704d0c35094594
Lormaniac <sean.lorman@gmail.com>
2019-06-12 20:13:27 -0600

    Created a variable named myName

---
+++ 5-Introduction-to-JavaScript/2-Declare-JavaScript-Variables.js
@@ -0,0 +1,6 @@
+// You can store data in variables. You declare a variable with var and then the name of the variable. There are 7 types of data types in JS.
+// Example
+var ourName;
+
+// Declare myName below this line
+var myName;
