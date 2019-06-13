6c0cb9c063c394bfadb4a24d2dea84a05abcffa6
Lormaniac <sean.lorman@gmail.com>
2019-06-12 20:37:49 -0600

    Gave our variables values appropriatly so we don't get undefined values

---
+++ 5-Introduction-to-JavaScript/5-Understanding-Uninitialized-Variables.js
@@ -0,0 +1,11 @@
+// If you don't give a value to a variable it is initially given the value of undefinied. This is appear in different ways when doing different operations. You have to make sure to only add numbers with numbers as you can't add a number to a string
+// Initialize these three variables
+var a = 5;
+var b = 10;
+var c = "I am a";
+
+// Do not change code below this line
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
