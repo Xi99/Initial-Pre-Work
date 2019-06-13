5002712fb59d6e005e3ddbc1642dfb12bf87600f
Lormaniac <sean.lorman@gmail.com>
2019-06-12 22:39:56 -0600

    Applied the escape method to add a literal quote to our string

---
+++ 5-Introduction-to-JavaScript/22-Escaping-Literal-Quotes-in-Strings.js
@@ -0,0 +1,2 @@
+// In order to use a literal quote inside of a string, you need to put it in \"...\" and it will appear how you mean it when it gets printed to the console
+var myStr = "I am a \"double quoted\" string inside \"double quotes\"." ; // Change this line
