0db14383b327115ea5bc278927c65538de590c66
Lormaniac <sean.lorman@gmail.com>
2019-06-12 20:32:09 -0600

    Assinged our variable a = 9 on same line it is declared

---
+++ 5-Introduction-to-JavaScript/4-Initializing-Variables-with-the-Assignment-Operator.js
@@ -0,0 +1,6 @@
+// It is common practice to initialize a variable to an initial value on the same line that it is declared
+// Example
+var ourVar = 19;
+
+// Only change code below this line
+var a = 9;
