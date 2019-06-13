9544e0bf191790c262ac2a76b32d2d57125e99c6
Lormaniac <sean.lorman@gmail.com>
2019-06-12 20:46:59 -0600

    Modified to give our declarations and assignments proper camelCase

---
+++ 5-Introduction-to-JavaScript/6-Understanding-Case-Sensitivity-in-Variables.js
@@ -0,0 +1,10 @@
+// Variable are case sensitive. Best practice is to use camelCase where the first word is lowercase and every subsequent word is capitalized
+// Declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
