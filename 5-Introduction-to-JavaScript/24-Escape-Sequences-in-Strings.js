8b44fa6e0c708372c8db70398afc55e491ba5e3b
Lormaniac <sean.lorman@gmail.com>
2019-06-12 23:18:31 -0600

    Applied multiple escape sequences to a string

---
+++ 5-Introduction-to-JavaScript/24-Escape-Sequences-in-Strings.js
@@ -0,0 +1,2 @@
+// You can use other escape sequences in strings to keep the string all on one line of code. You can do things like create a newline, tab, and backspace all with simple codes.
+var myStr = "FirstLine\n\t\/SecondLine\nThirdLine" ; // Change this line
