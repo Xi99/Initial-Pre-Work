e43ae3a51a97e4a18f9aa726bf02bd7b1dae266b
Lormaniac <sean.lorman@gmail.com>
2019-06-13 22:00:51 -0600

    Used the += operator to combine variables containing strings

---
+++ 5-Introduction-to-JavaScript/28-Appending_Variables_to_Strings.js
@@ -0,0 +1,11 @@
+// The "+=" operator allows you to append or add a variable that contains a string to another variable that contains a string to form one continous string
+// Example
+var anAdjective = "awesome!";
+var ourStr = "freeCodeCamp is ";
+ourStr += anAdjective;
+
+// Only change code below this line
+
+var someAdjective = "darn fun";
+var myStr = "Learning to code is ";
+myStr += someAdjective;
