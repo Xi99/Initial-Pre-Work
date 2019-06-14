483a692fabd7f2938e84f0f555dc2d87c8167328
Lormaniac <sean.lorman@gmail.com>
2019-06-13 22:54:14 -0600

    Found the [0] index of our lastName variable

---
+++ 5-Introduction-to-JavaScript/30-Use_Bracket_Notation_to_Find_the_First_Character_in_a_String.js
@@ -0,0 +1,13 @@
+// You can find the index of a string by using bracket notation. JS starts at 0 and goes up from there.
+// Example
+var firstLetterOfFirstName = "";
+var firstName = "Ada";
+
+firstLetterOfFirstName = firstName[0];
+
+// Setup
+var firstLetterOfLastName = "";
+var lastName = "Lovelace";
+
+// Only change code below this line
+firstLetterOfLastName = lastName[0];
