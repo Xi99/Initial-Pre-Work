ee1f401299c1114f50e96d059615156597c38237
Lormaniac <sean.lorman@gmail.com>
2019-06-13 23:00:14 -0600

    Changed the value of our myStr variable to "Hello World"

---
+++ 5-Introduction-to-JavaScript/31-Understand_String_Immutability.js
@@ -0,0 +1,7 @@
+// strings cannot have individual characters altered once it is made. You would need to change the whole string to a new string.
+// Setup
+var myStr = "Jello World";
+
+// Only change code below this line
+
+myStr = "Hello World"; // Fix Me
