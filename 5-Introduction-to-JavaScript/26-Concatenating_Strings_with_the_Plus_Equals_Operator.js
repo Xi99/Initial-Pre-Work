f569f5428e246641f9b7c4f781153309bfcf650b
Lormaniac <sean.lorman@gmail.com>
2019-06-13 20:53:11 -0600

    Concatenated two seperate strings into one string

---
+++ 5-Introduction-to-JavaScript/26-Concatenating_Strings_with_the_Plus_Equals_Operator.js
@@ -0,0 +1,9 @@
+// You can concatenate a new string to the end of an existing string by using the "+=" operator
+// Example
+var ourStr = "I come first. ";
+ourStr += "I come second.";
+
+// Only change code below this line
+
+var myStr = "This is the first sentence. ";
+myStr += "This is the second sentence."
