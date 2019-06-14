5d4fcf9ca96c8c0f74566447b6f71f8c48ea13b1
Lormaniac <sean.lorman@gmail.com>
2019-06-13 21:08:10 -0600

    Placed our myName variable between two strings using the concatentation operator

---
+++ 5-Introduction-to-JavaScript/27-Constructing_Strings_with_Variables.js
@@ -0,0 +1,8 @@
+// Use the concatenation operator "+" to add variables inbetween strings
+// Example
+var ourName = "freeCodeCamp";
+var ourStr = "Hello, our name is " + ourName + ", how are you?";
+
+// Only change code below this line
+var myName = "Sean Lorman";
+var myStr = "My name is " + myName + " and I am well!";
