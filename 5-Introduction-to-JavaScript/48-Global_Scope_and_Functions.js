bcc3eabee15465c3c5cffdba700862ae0ea88545
Lormaniac <sean.lorman@gmail.com>
2019-06-15 08:21:50 -0600

    Declared two global variable, one outside of a function and one in a function

---
+++ 5-Introduction-to-JavaScript/48-Global_Scope_and_Functions.js
@@ -0,0 +1,20 @@
+// Global variables can be used throughout your code. The scope of a global variable reaches everywhere but when you declare a variable in a function with var then it's scope is just within that function.
+// Declare your variable here
+var myGlobal = 10;
+
+function fun1() {
+  // Assign 5 to oopsGlobal Here
+  oopsGlobal = 5;
+}
+
+// Only change code above this line
+function fun2() {
+  var output = "";
+  if (typeof myGlobal != "undefined") {
+    output += "myGlobal: " + myGlobal;
+  }
+  if (typeof oopsGlobal != "undefined") {
+    output += " oopsGlobal: " + oopsGlobal;
+  }
+  console.log(output);
+}
