56271817cbc611874a5aedcabb10c5f2abd7f4dc
Lormaniac <sean.lorman@gmail.com>
2019-06-15 08:53:01 -0600

    Wrote function to change the value of our global variable but didn't return a value

---
+++ 5-Introduction-to-JavaScript/52-Understanding_Undefined_Value_returned_from_a_Function.js
@@ -0,0 +1,15 @@
+// Global variables can have their values changed within functions, but when that function is called without a return statement then it will come back undefined.
+// Example
+var sum = 0;
+function addThree() {
+  sum = sum + 3;
+}
+
+// Only change code below this line
+function addFive() {
+  sum += 5;
+}
+
+
+// Only change code above this line
+var returnedValue = addFive();
