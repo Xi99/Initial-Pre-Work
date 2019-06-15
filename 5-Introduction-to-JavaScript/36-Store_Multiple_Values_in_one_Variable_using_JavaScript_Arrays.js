b7eae743311ebd0035330dc0b6584ad2f20364e6
Lormaniac <sean.lorman@gmail.com>
2019-06-14 21:13:01 -0600

    Added a string and a number to our myArray variable

---
+++ 5-Introduction-to-JavaScript/36-Store_Multiple_Values_in_one_Variable_using_JavaScript_Arrays.js
@@ -0,0 +1,6 @@
+// An array variable can story many different types of data in it by seperating each with a comma
+// Example
+var ourArray = ["John", 23];
+
+// Only change code below this line.
+var myArray = ["Timothy", 213];
