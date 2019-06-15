17329dfe202eff46fd6dc8475837f73c4678130e
Lormaniac <sean.lorman@gmail.com>
2019-06-15 08:44:32 -0600

    Overrode our global variable in our function with a local variable

---
+++ 5-Introduction-to-JavaScript/50-Global_vs._Local_Scope_in_Functions.js
@@ -0,0 +1,14 @@
+// Local and Global variables can have the same name, but the local variable will override the global within a function.
+// Setup
+var outerWear = "T-Shirt";
+
+function myOutfit() {
+  // Only change code below this line
+
+  var outerWear = "sweater";
+
+  // Only change code above this line
+  return outerWear;
+}
+
+myOutfit();
