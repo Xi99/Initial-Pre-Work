e1a13b3e63f1096780ddbc7091fc471bb98ffd1a
Lormaniac <sean.lorman@gmail.com>
2019-06-14 21:28:05 -0600

    Accessed the first value of our myArray variable and set it to the value of myData

---
+++ 5-Introduction-to-JavaScript/38-Access_Array_Data_with_Indexes.js
@@ -0,0 +1,10 @@
+// You use the indexes of an array to access the data inside them. This is similar to the characters in the strings where it starts counting from 0
+// Example
+var ourArray = [50,60,70];
+var ourData = ourArray[0]; // equals 50
+
+// Setup
+var myArray = [50,60,70];
+
+// Only change code below this line.
+var myData = myArray[0];
