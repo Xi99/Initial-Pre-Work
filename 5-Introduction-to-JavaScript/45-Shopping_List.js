6525753cbfa92dd81faf7130c37b3eba10f0f998
Lormaniac <sean.lorman@gmail.com>
2019-06-14 22:58:55 -0600

    Made a shopping list with sub-arrays in a multi-dimensional array

---
+++ 5-Introduction-to-JavaScript/45-Shopping_List.js
@@ -0,0 +1,2 @@
+// You can create many uses for sub-arrays in a multi-dimensional array such as an item and a quantity of that item.
+var myList = [["Milk", 2], ["Cookies", 10], ["Marshmellows", 10], ["Sour Patch", 100], ["Apples", 45]];
