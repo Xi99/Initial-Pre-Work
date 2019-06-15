c512cd0a9263ee7f38c125bd31704ab75950b438
Lormaniac <sean.lorman@gmail.com>
2019-06-14 23:02:35 -0600

    Created the function reusableFunction() and called it

---
+++ 5-Introduction-to-JavaScript/46-Write_Reusable_JavaScript_with_Functions.js
@@ -0,0 +1,14 @@
+// A function can be reusable lines of code. Once you create the function you can call it whenever you want an it will execute everythign between the curly braces.
+// Example
+function ourReusableFunction() {
+  console.log("Heyya, World");
+}
+
+ourReusableFunction();
+
+// Only change code below this line
+function reusableFunction() {
+    console.log("Hi World")
+}
+
+reusableFunction();
