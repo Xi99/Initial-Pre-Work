e56489ecef215679fe93a80f438606fab84a4d3d
Lormaniac <sean.lorman@gmail.com>
2019-06-14 20:36:31 -0600

    Got the second to last character's index value of our lastName variable

---
+++ 5-Introduction-to-JavaScript/34-Use_Bracket_Notation_to_Find_the_Nth-to-Last_Character_in_a_String.js
@@ -0,0 +1,10 @@
+// You can use the same formula as the previous lesson to find the nth-to-last character in a variable by increasing the number you subtract
+// Example
+var firstName = "Ada";
+var thirdToLastLetterOfFirstName = firstName[firstName.length - 3];
+
+// Setup
+var lastName = "Lovelace";
+
+// Only change code below this line
+var secondToLastLetterOfLastName = lastName[lastName.length - 2];
