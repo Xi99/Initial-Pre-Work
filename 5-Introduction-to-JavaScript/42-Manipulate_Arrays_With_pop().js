bad53a0f08353052816a3e0b047b2f9320c36d36
Lormaniac <sean.lorman@gmail.com>
2019-06-14 22:27:23 -0600

    Removed the last index from our myArray via the .pop()

---
+++ 5-Introduction-to-JavaScript/42-Manipulate_Arrays_With_pop().js
@@ -0,0 +1,11 @@
+// the .pop() function removes the last index of an array, whether it be number, string, or array. You can also store this value in a variable.
+// Example
+var ourArray = [1,2,3];
+var removedFromOurArray = ourArray.pop();
+// removedFromOurArray now equals 3, and ourArray now equals [1,2]
+
+// Setup
+var myArray = [["John", 23], ["cat", 2]];
+
+// Only change code below this line.
+var removedFromMyArray = myArray.pop();
