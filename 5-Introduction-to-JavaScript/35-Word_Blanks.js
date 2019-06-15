edc552913636cd50eef8389ac7220daba3f0a2f7
Lormaniac <sean.lorman@gmail.com>
2019-06-14 21:06:02 -0600

    Concatenated variables and strings to form a Mad Libs sentence

---
+++ 5-Introduction-to-JavaScript/35-Word_Blanks.js
@@ -0,0 +1,11 @@
+// Make sure to add spaces when you are concatenate as those aren't automatically added.
+function wordBlanks(myNoun, myAdjective, myVerb, myAdverb) {
+  // Your code below this line
+  var result = "The " + myNoun + " is very " + myAdjective + " and can " + myVerb + " very " + myAdverb;
+
+  // Your code above this line
+  return result;
+}
+
+// Change the words here to test your function
+wordBlanks("dog", "big", "ran", "quickly");
