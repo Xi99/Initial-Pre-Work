939ee5c82f89cb5b1bed8d9c08bae414b1d64af3
Lormaniac <sean.lorman@gmail.com>
2019-06-14 19:39:18 -0600

    Used bracket notation to get a variable to equal the third letter of our lastName variable

---
+++ 5-Introduction-to-JavaScript/32-Use_Bracket_Notation_to_Find_the_Nth_Character_in_a_String.js
@@ -0,0 +1,10 @@
+// Computers start counting at 0 so in order to get a letter in a string at the index of 3, it would be [2]
+// Example
+var firstName = "Ada";
+var secondLetterOfFirstName = firstName[1];
+
+// Setup
+var lastName = "Lovelace";
+
+// Only change code below this line.
+var thirdLetterOfLastName = lastName[2];
