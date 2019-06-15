de0818bf809bdefdfe12851345ab55770e867ed0
Lormaniac <sean.lorman@gmail.com>
2019-06-14 20:18:39 -0600

    Changed the value of a variable to the last index character of our lastName variable

---
+++ 5-Introduction-to-JavaScript/33-Use_Bracket_Notation_to_Find_the_Last_Character_in_a_String.js
@@ -0,0 +1,10 @@
+// In order to get the last letter of a string guaranteed is to apply [variable.length - 1]
+// Example
+var firstName = "Ada";
+var lastLetterOfFirstName = firstName[firstName.length - 1];
+
+// Setup
+var lastName = "Lovelace";
+
+// Only change code below this line.
+var lastLetterOfLastName = lastName[lastName.length - 1];
