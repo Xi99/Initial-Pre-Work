ff295f8e7c6159e67bf33b1cca0062d30d805613
Lormaniac <sean.lorman@gmail.com>
2019-06-14 22:31:51 -0600

    Removed the first index value of our myArray and put it into a variable

---
+++ 5-Introduction-to-JavaScript/43-Manipulate_Arrays_With_shift().js
@@ -0,0 +1,11 @@
+// The .shift() removes the first index of an array. You can also put this value into a variable.
+// Example
+var ourArray = ["Stimpson", "J", ["cat"]];
+var removedFromOurArray = ourArray.shift();
+// removedFromOurArray now equals "Stimpson" and ourArray now equals ["J", ["cat"]].
+
+// Setup
+var myArray = [["John", 23], ["dog", 3]];
+
+// Only change code below this line.
+var removedFromMyArray = myArray.shift();
