9036baa9cc595a68608233a5b09f64ae4e604612
Lormaniac <sean.lorman@gmail.com>
2019-06-15 08:45:06 -0600

    Created a function to multiply a number by 5 and then called it

---
+++ 5-Introduction-to-JavaScript/51-Return_a_Value_from_a_Function_with_Return.js
@@ -0,0 +1,13 @@
+// The return statement can send a value back out of a function. You call the function outside of it with an argument for it to perform the function task.
+// Example
+function minusSeven(num) {
+  return num - 7;
+}
+
+// Only change code below this line
+function timesFive(num) {
+  return num * 5;
+}
+
+console.log(timesFive(5));
+console.log(minusSeven(10));
