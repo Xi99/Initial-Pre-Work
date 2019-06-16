062d7f4cad9d12c2dfd7b0e028a2ee4b78e03124
Lormaniac <sean.lorman@gmail.com>
2019-06-16 11:18:47 -0600

    Created a card counting function

---
+++ 5-Introduction-to-JavaScript/79-Counting_Cards.js
@@ -0,0 +1,33 @@
+// Using the switch and if / else if statement, you can make programs to do many calculations depending on the information given
+var count = 0;
+
+function cc(card) {
+  // Only change code below this line
+  switch(card){
+        case 2:
+        case 3:
+        case 4:
+        case 5:
+        case 6:
+          count++;
+          break;
+        case 10:
+        case "J":
+        case "Q":
+        case "K":
+        case "A":
+          count--;
+          break;
+      }
+      if (count > 0){
+        return count + " Bet";
+      } else {
+        return count + " Hold";
+      }
+
+  // Only change code above this line
+}
+
+// Add/remove calls to test your function.
+// Note: Only the last will display
+cc(2); cc(3); cc(7); cc('K'); cc('A');
