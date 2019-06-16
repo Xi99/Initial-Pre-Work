965376d08b9338a3abc69c46f971dd2dccd7f4a3
Lormaniac <sean.lorman@gmail.com>
2019-06-16 16:50:51 -0600

    We counted backwards by using a For loop and only pushed the odd numbers starting with 9 and going down

--- 5-Introduction-to-JavaScript/96-Count_Backwards_With_a_For_Loop.js
+++ 5-Introduction-to-JavaScript/96-Count_Backwards_With_a_For_Loop.js
@@ -1,1 +1,15 @@
+// You can setup your For loop to count backwards if you wanted by making the setup value higher and the while portion to be as long as it is greater than 0. You can then decrease from the initail i value.
+// Example
+var ourArray = [];
 
+for (var i = 10; i > 0; i -= 2) {
+  ourArray.push(i);
+}
+
+// Setup
+var myArray = [];
+
+// Only change code below this line.
+for (var i = 9; i > 0; i -= 2) {
+  myArray.push(i);
+}
