f665935c816faab2c7f99b5b164d62bce3d54853
Lormaniac <sean.lorman@gmail.com>
2019-06-15 19:08:29 -0600

    Changed our return from a boolean false, to a boolean true

---
+++ 5-Introduction-to-JavaScript/55-Understanding_Boolean_Values.js
@@ -0,0 +1,9 @@
+// Boolean is another data type that signifies either true or false. The value is never in quotes.
+function welcomeToBooleans() {
+
+// Only change code below this line.
+
+return true; // Change this line
+
+// Only change code above this line.
+}
