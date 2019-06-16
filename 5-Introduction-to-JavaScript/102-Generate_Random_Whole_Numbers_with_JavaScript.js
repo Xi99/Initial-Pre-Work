d4cb5fb425c998ec651a450cd5820fd7a17fb272
Lormaniac <sean.lorman@gmail.com>
2019-06-16 17:52:59 -0600

    Created a function to get a random whole number between 0 and 9

---
+++ 5-Introduction-to-JavaScript/102-Generate_Random_Whole_Numbers_with_JavaScript.js
@@ -0,0 +1,9 @@
+// We can use the Math.random() and multiply it to get numbers above 0. We can then put that result in the Math.floor() to get the number to round down to the nearest whole number.
+var randomNumberBetween0and19 = Math.floor(Math.random() * 20);
+
+function randomWholeNum() {
+
+  // Only change code below this line.
+
+  return Math.floor(Math.random() * 10);
+}
