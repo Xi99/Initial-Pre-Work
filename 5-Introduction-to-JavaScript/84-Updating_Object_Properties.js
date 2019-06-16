6222b9effc1ee2925319cba2acab3d4e6432c5f3
Lormaniac <sean.lorman@gmail.com>
2019-06-16 12:12:50 -0600

    Used dot notation to change our dogs name to "Happy Coder"

---
+++ 5-Introduction-to-JavaScript/84-Updating_Object_Properties.js
@@ -0,0 +1,21 @@
+// You can update properties in an object whenever you want since it is mutable. You can use either the dot or bracket notation
+// Example
+var ourDog = {
+  "name": "Camper",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["everything!"]
+};
+
+ourDog.name = "Happy Camper";
+
+// Setup
+var myDog = {
+  "name": "Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"]
+};
+
+// Only change code below this line.
+myDog.name = "Happy Coder";
