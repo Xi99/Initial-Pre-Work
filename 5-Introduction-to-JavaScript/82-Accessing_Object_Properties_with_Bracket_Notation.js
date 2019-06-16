d8a204c7271d2b82a3aed0be9dad66b62b993695
Lormaniac <sean.lorman@gmail.com>
2019-06-16 11:31:11 -0600

    Accessed data in our testOBj via the bracket notation

---
+++ 5-Introduction-to-JavaScript/82-Accessing_Object_Properties_with_Bracket_Notation.js
@@ -0,0 +1,12 @@
+// If the property has a space in it's name then you will have to use the bracket notation to access the data.
+// Setup
+var testObj = {
+  "an entree": "hamburger",
+  "my side": "veggies",
+  "the drink": "water"
+};
+
+// Only change code below this line
+
+var entreeValue = testObj ["an entree"];   // Change this line
+var drinkValue = testObj ["the drink"]  ;    // Change this line
