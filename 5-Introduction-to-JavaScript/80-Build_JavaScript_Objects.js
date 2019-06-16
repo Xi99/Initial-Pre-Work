8e95ccfe7944b3c2bec579559954a929f6822e2c
Lormaniac <sean.lorman@gmail.com>
2019-06-16 11:26:16 -0600

    Created a myDog object with data about a dog

---
+++ 5-Introduction-to-JavaScript/80-Build_JavaScript_Objects.js
@@ -0,0 +1,20 @@
+// Objects are another way to store data. You can access the data through properties. Objects are more structured.
+// Example
+var ourDog = {
+  "name": "Camper",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["everything!"]
+};
+
+// Only change code below this line.
+
+var myDog = {
+  name: "Cato",
+  legs: 4,
+  tails: 1,
+  friends: ["Sean Lorman", "Chris Lorman"]
+
+
+
+};
