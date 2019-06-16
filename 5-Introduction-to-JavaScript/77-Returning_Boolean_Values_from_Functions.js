ca7774e832998aadacd5ccac9a86e7869bf4225d
Lormaniac <sean.lorman@gmail.com>
2019-06-16 11:05:03 -0600

    Refactored our code to a simple boolean comparison

---
+++ 5-Introduction-to-JavaScript/77-Returning_Boolean_Values_from_Functions.js
@@ -0,0 +1,9 @@
+// You can refactor your code for if / else statements with boolean valuse with a comparison operator.
+function isLess(a, b) {
+  // Fix this code
+  return a < b;
+
+}
+
+// Change these values to test
+isLess(10, 15);
