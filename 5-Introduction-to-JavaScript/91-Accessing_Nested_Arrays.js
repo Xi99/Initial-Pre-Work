ae2f82613c992b16c63ab54d8a733e74bace6f37
Lormaniac <sean.lorman@gmail.com>
2019-06-16 13:27:20 -0600

    Accessed our nested array information using dot and bracket notation and put that information in a variable

---
+++ 5-Introduction-to-JavaScript/91-Accessing_Nested_Arrays.js
@@ -0,0 +1,24 @@
+// Using arracy bracket notation you can access nested arrays values. You will have to use the indices of the object location.
+// Setup
+var myPlants = [
+  {
+    type: "flowers",
+    list: [
+      "rose",
+      "tulip",
+      "dandelion"
+    ]
+  },
+  {
+    type: "trees",
+    list: [
+      "fir",
+      "pine",
+      "birch"
+    ]
+  }
+];
+
+// Only change code below this line
+
+var secondTree = myPlants[1].list[1];; // Change this line
