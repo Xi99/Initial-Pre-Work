68170252261f8b144a6ec2b025a37dba35d2ffaf
Lormaniac <sean.lorman@gmail.com>
2019-06-16 13:06:22 -0600

    Added another object to our myMusic array

---
+++ 5-Introduction-to-JavaScript/89-Manipulating_Complex_Objects.js
@@ -0,0 +1,23 @@
+// A complex data structure is an array that contains at least one object inside of it and allow for combinations of all the data types.
+var myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  },
+  {
+    "artist": "Hobo Johnson",
+    "title": "Peach Scones",
+    "release_year": 2018,
+    "formats": [
+      "CD",
+      "MP3",
+    ]
+  }
+];
