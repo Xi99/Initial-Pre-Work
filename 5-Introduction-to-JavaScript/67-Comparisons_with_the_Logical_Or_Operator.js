13faff88eee593da2d1a0ad2b36180d5cb59b6f3
Lormaniac <sean.lorman@gmail.com>
2019-06-15 21:48:35 -0600

    Refactored our code by deleting an if statement and combined it with another using the logical or operator

---
+++ 5-Introduction-to-JavaScript/67-Comparisons_with_the_Logical_Or_Operator.js
@@ -0,0 +1,15 @@
+// The logical or operator "||" is used to signify if one or the other operand is true, then it returns true.
+function testLogicalOr(val) {
+  // Only change code below this line
+
+  if (val < 10 || val > 20) {
+    return "Outside";
+
+  }
+
+  // Only change code above this line
+  return "Inside";
+}
+
+// Change this value to test
+testLogicalOr(15);
