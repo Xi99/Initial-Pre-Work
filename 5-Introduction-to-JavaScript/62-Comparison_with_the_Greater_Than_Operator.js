3c12cdee8f376c7b8f0795b2b1cc6650af6654ec
Lormaniac <sean.lorman@gmail.com>
2019-06-15 20:57:19 -0600

    Applied the greater than operator to compare some values

---
+++ 5-Introduction-to-JavaScript/62-Comparison_with_the_Greater_Than_Operator.js
@@ -0,0 +1,15 @@
+// The greater than operator ">" compares values of two numbers. If the one on the left is greater, than it returns true and vice versa
+function testGreaterThan(val) {
+  if (val > 100) {  // Change this line
+    return "Over 100";
+  }
+
+  if (val > 10) {  // Change this line
+    return "Over 10";
+  }
+
+  return "10 or Under";
+}
+
+// Change this value to test
+testGreaterThan(10);
