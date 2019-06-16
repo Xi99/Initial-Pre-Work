39e9ccc6e89fb30f26c888482bd1bc3b3d8c8d6a
Lormaniac <sean.lorman@gmail.com>
2019-06-16 16:43:47 -0600

    Iterated odd numbers with a for loop

---
+++ 5-Introduction-to-JavaScript/95-Iterate_Odd_Numbers_With_a_For_Loop.js
@@ -0,0 +1,15 @@
+// You can iterate odd numbers with a for loop by starting the setup to 1 and then having it incriment by 2 from there. You can do a similar setup for even numbers.
+// Example
+var ourArray = [];
+
+for (var i = 0; i < 10; i += 2) {
+  ourArray.push(i);
+}
+
+// Setup
+var myArray = [];
+
+// Only change code below this line.
+for (var i = 1; i < 10; i += 2) {
+  myArray.push(i);
+}
