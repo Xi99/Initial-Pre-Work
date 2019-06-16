2617ecc96866e23834e5c1ee9137c25e54149891
Lormaniac <sean.lorman@gmail.com>
2019-06-15 21:03:56 -0600

    Added the less than operator to our function to test their values

---
+++ 5-Introduction-to-JavaScript/64-Comparison_with_the_Less_Than_Operator.js
@@ -0,0 +1,15 @@
+// The less than operator "<" compares two numbers and if the number on the left is lower than the number on the right than the returned value is true.
+function testLessThan(val) {
+  if (val < 25) {  // Change this line
+    return "Under 25";
+  }
+
+  if (val < 55) {  // Change this line
+    return "Under 55";
+  }
+
+  return "55 or Over";
+}
+
+// Change this value to test
+testLessThan(10);
