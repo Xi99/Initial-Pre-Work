29e19c36d41217d4feff09c30e229e032efeb30d
Lormaniac <sean.lorman@gmail.com>
2019-06-15 20:01:30 -0600

    testEqual() used to compare values to see if they are true or false

---
+++ 5-Introduction-to-JavaScript/57-Comparison_with_the_Equality_Operator.js
@@ -0,0 +1,11 @@
+// The equality operator "==" compares two values and will return either true if they are equivalent and false if they aren't. Data types are converted by JS so they can be compared.
+// Setup
+function testEqual(val) {
+  if (val == 12) { // Change this line
+    return "Equal";
+  }
+  return "Not Equal";
+}
+
+// Change this value to test
+testEqual(12);
