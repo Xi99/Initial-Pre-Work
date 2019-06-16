1505d1b696b56594c6cbb35f5a12a5437f9c6750
Lormaniac <sean.lorman@gmail.com>
2019-06-16 13:19:40 -0600

    Assigned our variable gloveBoxContents to the value of our sup-property "glove box" in our myStorage object

---
+++ 5-Introduction-to-JavaScript/90-Accessing_Nested_Objects.js
@@ -0,0 +1,15 @@
+// Using the dot and bracket notation you can access data such as sub-properties nested within objecst
+// Setup
+var myStorage = {
+  "car": {
+    "inside": {
+      "glove box": "maps",
+      "passenger seat": "crumbs"
+     },
+    "outside": {
+      "trunk": "jack"
+    }
+  }
+};
+
+var gloveBoxContents = myStorage.car.inside["glove box"]; // Change this line
