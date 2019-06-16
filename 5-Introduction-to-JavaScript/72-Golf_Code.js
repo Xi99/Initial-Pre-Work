9e0b9dfeefe2bfd26512d7cda98c69797c768784
Lormaniac <sean.lorman@gmail.com>
2019-06-15 23:04:19 -0600

    Made a function called golfScore to let the golfer know what number of strokes to the par of the hole he had to what it represents

---
+++ 5-Introduction-to-JavaScript/72-Golf_Code.js
@@ -0,0 +1,25 @@
+// The flow of your if / else if statements are very important. Also it is important to remember to use "==" when comparing two different data types.
+var names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home!"];
+function golfScore(par, strokes) {
+  // Only change code below this line
+  if (strokes == 1) {
+    return "Hole-in-one!";
+  } else if (strokes <= par - 2) {
+    return "Eagle";
+  } else if (strokes == par - 1) {
+    return "Birdie";
+  } else if (strokes == par) {
+    return "Par";
+  } else if (strokes == par + 1) {
+    return "Bogey";
+  } else if (strokes == par + 2) {
+    return "Double Bogey";
+  } else {
+    return "Go Home!";
+  }
+
+  // Only change code above this line
+}
+
+// Change these values to test
+golfScore(5, 4);
