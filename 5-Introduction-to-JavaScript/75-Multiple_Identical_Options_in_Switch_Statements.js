cde024e34dec3bdd1511174a534f1b62d7d169d5
Lormaniac <sean.lorman@gmail.com>
2019-06-15 23:57:40 -0600

    Used multiple identical options in our switch statement

--- 5-Introduction-to-JavaScript/75-Multiple_Identical_Options_in_Switch_Statements.js
+++ 5-Introduction-to-JavaScript/75-Multiple_Identical_Options_in_Switch_Statements.js
@@ -1,1 +1,29 @@
+//  You can put multiple case statements before the break if they are going to have the same output if those cases are executed.
+function sequentialSizes(val) {
+  var answer = "";
+  // Only change code below this line
+  switch (val) {
+    case 1:
+    case 2:
+    case 3:
+      return "Low";
+      break;
+    case 4:
+    case 5:
+    case 6:
+      return "Mid";
+      break;
+    case 7:
+    case 8:
+    case 9:
+      return "High";
+      break;
+  }
 
+
+  // Only change code above this line
+  return answer;
+}
+
+// Change this value to test
+sequentialSizes(1);
