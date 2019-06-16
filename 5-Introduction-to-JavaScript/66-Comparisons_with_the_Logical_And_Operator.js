b225c66ae7c65939dc5420a0e1c0d3c12d38f8c5
Lormaniac <sean.lorman@gmail.com>
2019-06-15 21:25:26 -0600

    Refactored our code by removing two "if" statements with an "&&" operator to compare two values

---
+++ 5-Introduction-to-JavaScript/66-Comparisons_with_the_Logical_And_Operator.js
@@ -0,0 +1,15 @@
+// the "&&" operator is able to compare two sets of comparisons on the same line.
+function testLogicalAnd(val) {
+  // Only change code below this line
+
+  if (val <= 50 && val >= 25) {
+      return "Yes";
+
+  }
+
+  // Only change code above this line
+  return "No";
+}
+
+// Change this value to test
+testLogicalAnd(10);
