c5b5bbeca1db8d4803fccae3715ea2a6c688edd0
Lormaniac <sean.lorman@gmail.com>
2019-06-16 16:57:28 -0600

    Iterated through our array to add up all the indices and put the sum in a variable called total

---
+++ 5-Introduction-to-JavaScript/97-Iterate_Through_an_Array_with_a_For_Loop.js
@@ -0,0 +1,17 @@
+// You can iterate through a whole array by using (arrayname).length and having it as long as i is less than that length. Then you can do things which involve the whole array like in this case where we are adding all the indices.
+// Example
+var ourArr = [ 9, 10, 11, 12];
+var ourTotal = 0;
+
+for (var i = 0; i < ourArr.length; i++) {
+  ourTotal += ourArr[i];
+}
+
+// Setup
+var myArr = [ 2, 3, 4, 5, 6];
+
+// Only change code below this line
+var total = 0;
+for (var i = 0; i < myArr.length; i++) {
+  total += myArr[i];
+}
