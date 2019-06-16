79d03fc541f1d4cb79226b82f4f105243390074b
Lormaniac <sean.lorman@gmail.com>
2019-06-16 16:26:07 -0600

    Created a While loop to iterate the conditions

---
+++ 5-Introduction-to-JavaScript/93-Iterate_with_JavaScript_While_Loops.js
@@ -0,0 +1,9 @@
+// The While loop keeps a certain set of tasks going until the condition is met.
+// Setup
+var myArray = [];
+var i = 0
+while(i < 5) {
+  myArray.push(i);
+  i++;
+}
+// Only change code below this line.
