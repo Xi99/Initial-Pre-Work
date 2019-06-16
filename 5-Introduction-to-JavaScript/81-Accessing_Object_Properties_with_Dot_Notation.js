7c65113b81ffdcac9b7795dbef02dd18807ba57d
Lormaniac <sean.lorman@gmail.com>
2019-06-16 11:28:37 -0600

    Accessed property values via the dot notation in our tesObj

---
+++ 5-Introduction-to-JavaScript/81-Accessing_Object_Properties_with_Dot_Notation.js
@@ -0,0 +1,12 @@
+// You can access the data inside of an object via the dot notation and bracket notation. For dot notation you would need to know the name of the property.
+// Setup
+var testObj = {
+  "hat": "ballcap",
+  "shirt": "jersey",
+  "shoes": "cleats"
+};
+
+// Only change code below this line
+
+var hatValue = testObj.hat;      // Change this line
+var shirtValue = testObj.shirt;    // Change this line
