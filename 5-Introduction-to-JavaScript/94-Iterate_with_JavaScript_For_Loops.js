6a66ffeb255446f3a253087bbffa686b14fdda72
Lormaniac <sean.lorman@gmail.com>
2019-06-16 16:40:22 -0600

    Used a For Loop to push numbers into our array until the conditions were no longer met

---
+++ 5-Introduction-to-JavaScript/94-Iterate_with_JavaScript_For_Loops.js
@@ -0,0 +1,15 @@
+// The For Loop is used to executed an iteration a certain amount of times specified. You have a setup value for the loop variable, then a condition that has to be true, and if that is true then the final step will execute.
+// Example
+var ourArray = [];
+
+for (var i = 0; i < 5; i++) {
+  ourArray.push(i);
+}
+
+// Setup
+var myArray = [];
+
+// Only change code below this line.
+for (var i = 1; i < 6; i++) {
+  myArray.push(i);
+}
