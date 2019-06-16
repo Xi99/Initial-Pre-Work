01c4eb888de34e398e6fdc283a9a0bde76470c55
Lormaniac <sean.lorman@gmail.com>
2019-06-15 20:12:49 -0600

    Added the strict equality operator to our function to see if the value and data type match

---
+++ 5-Introduction-to-JavaScript/59-Practice_comparing_different_values.js
@@ -0,0 +1,11 @@
+// The typeof operator can determin the type or value of a variable. You can use this to know if it is a string or number.
+// Setup
+function compareEquality(a, b) {
+  if (a === b) { // Change this line
+    return "Equal";
+  }
+  return "Not Equal";
+}
+
+// Change this value to test
+compareEquality(10, "10");
