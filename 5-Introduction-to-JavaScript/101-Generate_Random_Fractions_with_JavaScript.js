77d84613543f03dca32116d4032eb586d3ad966b
Lormaniac <sean.lorman@gmail.com>
2019-06-16 17:44:52 -0600

    Made a function that returned Math.random()

---
+++ 5-Introduction-to-JavaScript/101-Generate_Random_Fractions_with_JavaScript.js
@@ -0,0 +1,9 @@
+// The Math.random() function returns a random decimal number between 0 (inclusive) and not quite up to 1 (exclusive)
+function randomFraction() {
+
+  // Only change code below this line.
+
+  return Math.random();
+
+  // Only change code above this line.
+}
