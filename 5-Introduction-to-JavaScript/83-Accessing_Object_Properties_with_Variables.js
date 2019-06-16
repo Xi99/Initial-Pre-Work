2a25354d22dcf1db2d280a974889691efb5a143b
Lormaniac <sean.lorman@gmail.com>
2019-06-16 12:10:25 -0600

    Accesed an objects properties using a variable to get a football players name by their jersey number

---
+++ 5-Introduction-to-JavaScript/83-Accessing_Object_Properties_with_Variables.js
@@ -0,0 +1,12 @@
+// We can access object properties using variables with bracket notation. You can then reuse the variable by changing one piece of data to get another outcome.
+// Setup
+var testObj = {
+  12: "Namath",
+  16: "Montana",
+  19: "Unitas"
+};
+
+// Only change code below this line;
+
+var playerNumber = 16;       // Change this Line
+var player = testObj[playerNumber];   // Change this Line
