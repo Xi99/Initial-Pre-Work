505fd5842a13b39e1a77b0b270e4b09f81a129d4
Lormaniac <sean.lorman@gmail.com>
2019-06-15 20:17:34 -0600

    Used the unequality operator to test our function call as being true or false

---
+++ 5-Introduction-to-JavaScript/60-Comparison_with_the_Inequality_Operator.js
@@ -0,0 +1,11 @@
+// The inequality operator "!=" is the opposite of the equality operator "==". It is to say if something "is not" equal, then it is true and vice versa.
+// Setup
+function testNotEqual(val) {
+  if (val != 99) { // Change this line
+    return "Not Equal";
+  }
+  return "Equal";
+}
+
+// Change this value to test
+testNotEqual(99);
