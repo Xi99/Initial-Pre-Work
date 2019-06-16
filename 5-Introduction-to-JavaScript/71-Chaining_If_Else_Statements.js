2c46888d6fca85685861757e6f4aed084396b41d
Lormaniac <sean.lorman@gmail.com>
2019-06-15 22:30:59 -0600

    Chained our if else statements together for complex logic

---
+++ 5-Introduction-to-JavaScript/71-Chaining_If_Else_Statements.js
@@ -0,0 +1,22 @@
+// You can chain as many if / else if statements as you want and then end it with an else {..}
+function testSize(num) {
+  // Only change code below this line
+  if (num < 5) {
+    return "Tiny";
+  } else if (num < 10) {
+    return "Small";
+  } else if (num < 15) {
+    return "Medium";
+  } else if (num < 20) {
+    return "Large";
+  } else if (num >= 20) {
+    return "Huge"
+  } else {
+    return "Change Me";
+  }
+
+  // Only change code above this line
+}
+
+// Change this value to test
+testSize(7);
