4e6e08bd190e25aa06cde8dd9e99899268e7c604
Lormaniac <sean.lorman@gmail.com>
2019-06-16 19:54:28 -0600

    Used arrow function syntax to complete our magic function

---
+++ 6-Introduction-to-the-ES6-Challenges/6-Use-Arrow-Functions-to-Write-Concise-Anonymous-Functions.js
@@ -0,0 +1,5 @@
+// The arrow function sytax provides us with allows us to not have to write anonymous functions like we have been doing.
+const magic = () => {
+  "use strict";
+  return new Date();
+};
