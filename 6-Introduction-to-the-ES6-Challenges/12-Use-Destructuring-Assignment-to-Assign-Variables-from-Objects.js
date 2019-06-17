a0a93e96961fdb00d156c2d39d40dd234a929df2
Lormaniac <sean.lorman@gmail.com>
2019-06-16 21:08:25 -0600

    Took the value of the property signifying tomorrow's weather and put than in a new variable

---
+++ 6-Introduction-to-the-ES6-Challenges/12-Use-Destructuring-Assignment-to-Assign-Variables-from-Objects.js
@@ -0,0 +1,15 @@
+// It applies similar applications as the spread operator, but for objects. You can take properties values and put them in another variable.
+const AVG_TEMPERATURES = {
+  today: 77.5,
+  tomorrow: 79
+};
+
+function getTempOfTmrw(avgTemperatures) {
+  "use strict";
+  // change code below this line
+  const {tomorrow : tempOfTomorrow } = avgTemperatures; // change this line
+  // change code above this line
+  return tempOfTomorrow;
+}
+
+console.log(getTempOfTmrw(AVG_TEMPERATURES)); // should be 79
