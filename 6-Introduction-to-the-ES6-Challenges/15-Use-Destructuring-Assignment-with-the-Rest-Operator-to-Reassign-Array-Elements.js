47bb114bf6ace55b6e860c00638246729882a37e
Lormaniac <sean.lorman@gmail.com>
2019-06-16 21:55:03 -0600

    Used destructuring assignment with the rest operator to get data from one array and put it in a variable

---
+++ 6-Introduction-to-the-ES6-Challenges/15-Use-Destructuring-Assignment-with-the-Rest-Operator-to-Reassign-Array-Elements.js
@@ -0,0 +1,12 @@
+// You can take a chunk of an array but listing out the parts you don't want seperated by commas, and then the parts you do by ...arr. You can then put that data into a variable where it will output them in an array
+const source = [1,2,3,4,5,6,7,8,9,10];
+function removeFirstTwo(list) {
+  "use strict";
+  // change code below this line
+  const [a, b, ...arr] = list; // change this
+  // change code above this line
+  return arr;
+}
+const arr = removeFirstTwo(source);
+console.log(arr); // should be [3,4,5,6,7,8,9,10]
+console.log(source); // should be [1,2,3,4,5,6,7,8,9,10];
