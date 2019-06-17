b4c8e69cde2dfd597d90680d388d77b46ddc41e3
Lormaniac <sean.lorman@gmail.com>
2019-06-16 22:17:06 -0600

    Took the min and max properties from our stats obj and used them as parameters in our half function

---
+++ 6-Introduction-to-the-ES6-Challenges/16-Use-Destructuring-Assignment-to-Pass-an-Object-as-a-Function-s-Parameters.js
@@ -0,0 +1,22 @@
+// You can put an object, or parts of another object, as the parameters of a funtion in order to use them in the body of the function.
+const stats = {
+  max: 56.78,
+  standard_deviation: 4.34,
+  median: 34.54,
+  mode: 23.87,
+  min: -0.75,
+  average: 35.85
+};
+const half = (function() {
+  "use strict"; // do not change this line
+
+  // change code below this line
+  return ({ max, min }) => {
+    // use function argument destructuring
+    return (stats.max + stats.min) / 2.0;
+  };
+  // change code above this line
+
+})();
+console.log(stats); // should be object
+console.log(half(stats)); // should be 28.015
