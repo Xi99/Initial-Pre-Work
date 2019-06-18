d01bfcd343418600534ff66394730dc66f98c7bf
Lormaniac <sean.lorman@gmail.com>
2019-06-17 20:58:34 -0600

    Used import * to bring in the files of "capitalize_strings" into our current file

---
+++ 6-Introduction-to-the-ES6-Challenges/24-Use-Asterisk-to-Import-Everything-from-a-File.js
@@ -0,0 +1,3 @@
+// Using * with the import statement allows you to import all the contents of a file into your current file.
+import * as myCapitalizationStuff from "capitalize_strings";
+"use strict";
