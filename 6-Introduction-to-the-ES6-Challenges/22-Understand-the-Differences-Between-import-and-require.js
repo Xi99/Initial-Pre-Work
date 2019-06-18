a2146b347356750021b5d3f731780f49fa06b03c
Lormaniac <sean.lorman@gmail.com>
2019-06-17 20:46:48 -0600

    Used the import statement to bring capitalizeString function from "string_functions"

---
+++ 6-Introduction-to-the-ES6-Challenges/22-Understand-the-Differences-Between-import-and-require.js
@@ -0,0 +1,4 @@
+// You can import specific funcions that are packed in within a larger file of functions by using the import statement.
+"use strict";
+capitalizeString("hello!");
+import { capitalizeString } from "string_functions"
