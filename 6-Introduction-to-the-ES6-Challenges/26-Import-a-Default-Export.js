1de8e38889ee7998d6b82ee8b67c723c905c0ea3
Lormaniac <sean.lorman@gmail.com>
2019-06-17 21:05:50 -0600

    Imported the export default method subtract from "math_functions"

---
+++ 6-Introduction-to-the-ES6-Challenges/26-Import-a-Default-Export.js
@@ -0,0 +1,4 @@
+// In order to import a export default module or file you use import without the {}. This allows you to just write the value's name after the import.
+import subtract from "math_functions";
+"use strict";
+subtract(7,4);
