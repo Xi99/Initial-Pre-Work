cf72039419cdbcddf4432ad96b9d0289ef9e243a
Lormaniac <sean.lorman@gmail.com>
2019-06-17 19:57:59 -0600

    Refactored our code to use the updated ES6 syntax for functions

---
+++ 6-Introduction-to-the-ES6-Challenges/19-Write-Concise-Declarative-Functions-with-ES6.js
@@ -0,0 +1,12 @@
+// With ES6 we can forego the function keyword alltogether
+// change code below this line
+const bicycle = {
+  gear: 2,
+  setGear(newGear) {
+    "use strict";
+    this.gear = newGear;
+  }
+};
+// change code above this line
+bicycle.setGear(3);
+console.log(bicycle.gear);
