847e39db4f69059d8604a1e1589e79fc54ed770d
Lormaniac <sean.lorman@gmail.com>
2019-06-17 21:02:46 -0600

    export defaulted our subtract module

---
+++ 6-Introduction-to-the-ES6-Challenges/25-Create-an-Export-Fallback-with-export-default.js
@@ -0,0 +1,3 @@
+// the export default is used to export one value from a file. It can only be used once in a file and can also act as fallback value for a file or module.
+"use strict";
+export default function subtract(x,y) {return x - y;}
